d8d5562c1520c4410ed53fea1258230d2a6ddb62
Shane <shane@duairc.com>
2017-03-23 14:04:24 +0000

    Added types-hashable orphan instance package

---
+++ types-hashable/src/Type/Meta/Hashable.hs
@@ -0,0 +1,63 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE GADTs #-}
+
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+#ifdef SafeHaskell
+{-# LANGUAGE Safe #-}
+#endif
+
+#ifdef DataPolyKinds
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE PolyKinds #-}
+#endif
+
+module Type.Meta.Hashable
+    ()
+where
+
+-- hashable ------------------------------------------------------------------
+import           Data.Hashable
+                     ( Hashable, hashWithSalt
+#if !MIN_VERSION_base(4, 7, 0)
+                     , hash
+#endif
+                     )
+import           Data.Hashable.Lifted (Hashable1, liftHashWithSalt)
+
+
+-- types ---------------------------------------------------------------------
+import           Type.Meta
+                     ( Sing (Sing), Some (Some), val
+#if !MIN_VERSION_base(4, 7, 0)
+                     , Proxy
+#endif
+                     )
+
+
+------------------------------------------------------------------------------
+instance Hashable (Sing r a) where
+    hashWithSalt s (Sing a) = hashWithSalt s a
+
+
+------------------------------------------------------------------------------
+instance Hashable r => Hashable (Some r) where
+    hashWithSalt s (Some (Sing a)) = hashWithSalt s (val a)
+
+
+------------------------------------------------------------------------------
+instance Hashable1 Some where
+    liftHashWithSalt hws s (Some (Sing a)) = hws s (val a)
+#if !MIN_VERSION_base(4, 7, 0)
+
+
+------------------------------------------------------------------------------
+instance Hashable (Proxy a) where
+    hash _ = 0
+    hashWithSalt s _ = s
+
+
+------------------------------------------------------------------------------
+instance Hashable1 Proxy where
+    liftHashWithSalt _ s _ = s
+#endif
